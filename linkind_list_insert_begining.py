d33aad7dad6b4626eb242da45ed2fd5af782b417
Arun Kumar Damera <109560413+adamera821@users.noreply.github.com>
2022-10-17 16:33:21 -0700

    Create linkind_list_insert_begining.py
    What is inside?
    1.Defining a Node class to hold the values and links between nodes
    2.Implementing a LinkedList class to handle external operations on the list like adding

---
+++ linkind_list_insert_begining.py
@@ -0,0 +1,41 @@
+''''A linked list is a sequence of data elements, which are connected together via links. Each data element contains a connection to another data element in form of a pointer. Python does not have linked lists in its standard library. We implement the concept of linked lists using the concept of nodes as discussed in the previous chapter.
+
+We have already seen how we create a node class and how to traverse the elements of a node.In this chapter we are going to study the types of linked lists known as singly linked lists. In this type of data structure there is only one link between any two data elements. We create such a list and create additional methods to insert, update and remove elements from the list.''''
+
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+    
+    def get_value(self):
+        return self.value
+    
+    def get_next_node(self):
+        return self.next_node
+    
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+    
+class Linkedlist:
+    def __init__(self, value=None):
+        self.head_value = Node(value)
+    
+    def get_head(self):
+        return self.head_value
+    
+    def inser_begining(self, new_value):
+        new_node = Node(new_value)
+        new_node.set_next_node(self.head_value)
+        self.head_value = new_node
+    
+    def stringify(self):
+        stringfying = ""
+        current_node = self.get_head()
+        while current_node:
+            if current_node.get_value() !=None:
+                stringfying += str(current_node.get_value()) + "\n"
+            current_node = current_node.get_next_node()
+        return stringfying
+
+ll = Linkedlist(80)
+ll.inser_begining(89)
